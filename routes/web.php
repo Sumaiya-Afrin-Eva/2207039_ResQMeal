ebabe8e44172a6b855eb583fe44f65f5d1789abb
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-07-05 13:38:24 +0600

    Configured Web Routes for Donations

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\NGOVolunteerController;
 
 Route::get('/', function () {
-    return view('home');
+    $donations = \App\Models\Donation::where('expiry', '>', now())
+                    ->latest()
+                    ->get();
+    return view('home', compact('donations'));
 });
 
 Route::get('/login', function () {
     return view('login');
 });
 
+
 Route::get('/donate', function () {
     return view('donate');
 });
+
+Route::post('/donate', [DonationController::class, 'store'])->name('donations.store');
