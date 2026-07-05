1b84202d065e8a5c582279635cc81a44f0b6964e
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-07-05 13:31:27 +0600

    Added Fillable Attributes to Donation Model

---
+++ app/Models/Donation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Donation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'donation';
+
+    protected $fillable = [
+        'food_name',
+        'category',
+        'quantity',
+        'unit',
+        'serves',
+        'expiry',
+        'pickup_from',
+        'pickup_to',
+        'pickup_address',
+        'pickup_contact',
+        'storage',
+        'packaging',
+        'allergens',
+        'dietary',
+        'notes',
+        'visibility',
+        'emergency',
+        'donor_name',
+    ];
+}
