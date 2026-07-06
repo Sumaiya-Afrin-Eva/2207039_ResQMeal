58e6ea7421750146adb9efadd653bf1659ff5ae7
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-07-06 20:42:49 +0600

    Configured Web Routes for NGO/Volunteer

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DonationController;
 use App\Http\Controllers\NGOVolunteerController;
+use App\Http\Controllers\FoodRequestController;
 
 Route::get('/', function () {
     $donations = \App\Models\Donation::where('expiry', '>', now())
@@ -15,9 +16,16 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/ngo-login', function () {
+    return view('ngo-login');
+});
+
+Route::post('/ngo-login-check', [NGOVolunteerController::class, 'login'])->name('ngo.login');
+Route::post('/ngo-register',    [NGOVolunteerController::class, 'store'])->name('ngo.register');
 
 Route::get('/donate', function () {
     return view('donate');
 });
 
 Route::post('/donate', [DonationController::class, 'store'])->name('donations.store');
+Route::get('/donation/{id}', [DonationController::class, 'show'])->name('donation.show');
