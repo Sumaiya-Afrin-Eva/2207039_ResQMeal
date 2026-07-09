6219c5740cd530c6c6a7f4a76163f00e224b489f
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-07-09 21:46:49 +0600

    Created Donor Table using Migration

---
+++ database/migrations/2026_07_06_150213_donor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donor', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('organisation')->nullable();
+            $table->string('city');
+            $table->enum('donor_type', ['individual', 'restaurant', 'event', 'other'])->default('individual');
+            $table->string('password');
+            $table->boolean('is_verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donor');
+    }
+};
