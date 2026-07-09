141573bf61066885926c885e09e54db1f992268b
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-07-09 22:04:37 +0600

    Updated Web Routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\DonationController;
 use App\Http\Controllers\NGOVolunteerController;
 use App\Http\Controllers\FoodRequestController;
 
+use App\Http\Controllers\DonorController;
+
 Route::get('/', function () {
     $donations = \App\Models\Donation::where('expiry', '>', now())
                     ->latest()
@@ -15,6 +17,9 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('/donor-login-check', [DonorController::class, 'login'])->name('donor.login');
+Route::post('/donor-register',    [DonorController::class, 'store'])->name('donor.register');
 
 Route::get('/ngo-login', function () {
     return view('ngo-login');
@@ -29,3 +34,29 @@ Route::get('/donate', function () {
 
 Route::post('/donate', [DonationController::class, 'store'])->name('donations.store');
 Route::get('/donation/{id}', [DonationController::class, 'show'])->name('donation.show');
+
+Route::get('/my-history', function () {
+    return view('donor-history');
+});
+
+Route::get('/donor-requests', function () {
+    return view('donor-requests');
+});
+
+Route::get('/api/donations/history', [DonationController::class, 'history']);
+Route::get('/api/donations/{id}', [DonationController::class, 'apiShow']);
+Route::put('/api/donations/{id}', [DonationController::class, 'update']);
+Route::delete('/api/donations/{id}', [DonationController::class, 'destroy']);
+
+// Donor Request Approval Routes
+Route::get('/api/donor/requests', [App\Http\Controllers\FoodRequestController::class, 'donorRequests']);
+Route::get('/api/ngo/requests', [App\Http\Controllers\FoodRequestController::class, 'ngoRequests']);
+Route::post('/api/requests/{id}/approve', [App\Http\Controllers\FoodRequestController::class, 'approve']);
+Route::post('/api/requests/{id}/reject', [App\Http\Controllers\FoodRequestController::class, 'reject']);
+
+Route::get('/request',  [FoodRequestController::class, 'create'])->name('food.request.form');
+Route::post('/request', [FoodRequestController::class, 'store'])->name('food.request.store');
+
+Route::get('/ngo-requests', function () {
+    return view('ngo-requests');
+});
